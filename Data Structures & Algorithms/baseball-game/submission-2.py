6bd212d0c5d8293f8eff30ababdc6490dd134011
ZBramblett <dukeman254@gmail.com>
2026-08-31 14:17:22 -0500

    Add: baseball-game - submission-2

---
+++ Data Structures & Algorithms/baseball-game/submission-2.py
@@ -0,0 +1,27 @@
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        sums = 0
+        record = []
+        for i in range(len(operations)):
+            if operations[i] == "+":
+                x = record.pop()
+                y = record.pop()
+                z = x + y
+                record.append(y)
+                record.append(x)
+                record.append(z)
+            elif operations[i] == "D":
+                x = record.pop()
+                z = (2 * x)
+                record.append(x)
+                record.append(z)
+            elif operations[i] == "C":
+                record.pop()
+            else:
+                record.append(int(operations[i]))
+
+        for i in range(len(record)):
+            sums += record.pop()
+
+        return sums
+        
